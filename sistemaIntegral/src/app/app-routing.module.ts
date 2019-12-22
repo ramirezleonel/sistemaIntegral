75337a3d09cad47e41b51646757fc0f251977f10
ramirezleonel <ramirezleonel0@gmail.com>
2019-12-22 10:42:24 -0300

    cambios en los texto del sitio

--- sistemaIntegral/src/app/app-routing.module.ts
+++ sistemaIntegral/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HeaderComponent } from './header/header.component';
 import { ServiciosComponent } from './servicios/servicios.component';
+import { GaleriaComponent } from './galeria/galeria.component';
 
 
 const routes: Routes = [
@@ -19,6 +20,10 @@ const routes: Routes = [
   {
     path:'header',
     component:HeaderComponent
+  },
+  {
+    path:'galeria',
+    component:GaleriaComponent
   },
   {
     path:'servicios',
