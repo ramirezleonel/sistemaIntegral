ec027466365839eee7dcf6df7240baa35bed8300
ramirezleonel <ramirezleonel0@gmail.com>
2019-12-22 01:25:13 -0300

    correción en el bug de la hamburguesita

--- sistemaIntegral/src/assets/js/si-script.js
+++ sistemaIntegral/src/assets/js/si-script.js
@@ -1,10 +1,24 @@
 $( document ).ready(function() {
-    new WOW().init();
-    $(".scroll").click(function(event){		
-        event.preventDefault();
-        $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+   //Hamburguer
+    var pull 		= $('#pull');
+        menu 		= $('nav ul');
+        menuHeight	= menu.height();
+    $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
     });
+    $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 320 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+ //End Hamburguer
+  $(".scroll").on('click',function(event){		
+            event.preventDefault();
+            $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+        });
+        $().UItoTop({ easingType: 'easeOutQuart' });
 
-    $().UItoTop({ easingType: 'easeOutQuart' });
-										
-  });
+        new WOW().init();
+    });
